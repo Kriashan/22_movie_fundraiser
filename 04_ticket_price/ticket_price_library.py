b2a9c1b41e4e9289820edfca9afc2af933d89e20
Eldon Salter <es19090@melville-high.school.nz>
2022-10-14 09:23:09 +1300

    Removed 2 ask_user functions

--- 04_ticket_price/ticket_price_library.py
+++ 04_ticket_price/ticket_price_library.py
@@ -10,22 +10,22 @@ def is_a_seat_available(seats_available) -> bool:
     return False
 
 # Return the users name
-def ask_user_for_first_name() -> str:
-    return ask_user_question('What is your first name?', '[a-zA-Z]')
+#def ask_user_for_first_name() -> str:
+#    return ask_user_question('What is your first name?', '[a-zA-Z]')
 
 # Return the users age
-def ask_user_for_age(min_user_age=12, max_user_age=130) -> int:
-    if is_number_in_range(user_answer := 
-                            ask_user_question('What is your age?',
-                            '[0-9]'),
-                            min_user_age,
-                            max_user_age):
-        return user_answer
-    sys.stdout.write("Your age must be an integer between {0} and {1}".format(min_user_age, max_user_age))
-    return ask_user_for_age(min_user_age, max_user_age)
+#def ask_user_for_age(min_user_age=12, max_user_age=130) -> int:
+#    if is_number_in_range(user_answer := 
+#                            ask_user_question('What is your age?',
+#                            '[0-9]'),
+#                            min_user_age,
+#                            max_user_age):
+#        return user_answer
+#    sys.stdout.write("Your age must be an integer between {0} and {1}".format(min_user_age, max_user_age))
+#    return ask_user_for_age(min_user_age, max_user_age)
 
 # Ask question until the answer is not blank
-def ask_user_question(question='', regex_character_filter='') -> str:
+def ask_user_question(question='', regex_character_filter=''):
     if (answer := ''.join(re.findall(regex_character_filter, input(question)))) != '':
         return answer
     sys.stdout.write('Input cannot be blank')
