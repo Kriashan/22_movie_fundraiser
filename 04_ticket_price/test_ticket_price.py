a362db53977fe6d6d8ebb3c3f07fb430048ae0c8
Eldon Salter <es19090@melville-high.school.nz>
2022-10-14 09:15:24 +1300

    Removed all functions from class

--- 04_ticket_price/test_ticket_price.py
+++ 04_ticket_price/test_ticket_price.py
@@ -1,7 +1,7 @@
-import ticket_price
+import ticket_price_library as ticket
 
 if __name__ == '__main__':
-    ticketLibrary = ticket_price.TicketSystem(seats_available=150)
+    ticketLibrary = ticket.TicketSystem(seats_available=150)
 
     #print(ticketLibrary.is_a_seat_available())
     print(ticketLibrary.ask_user_question('Am I cute?', '[A-Za-z]'))
