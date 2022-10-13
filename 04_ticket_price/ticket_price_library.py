fb8c365e6569bc29f13a0ca575dfddf5eb1b1c80
Eldon Salter <es19090@melville-high.school.nz>
2022-10-14 10:47:51 +1300

    More specific error message

--- 04_ticket_price/ticket_price_library.py
+++ 04_ticket_price/ticket_price_library.py
@@ -2,13 +2,6 @@ import sys
 import csv
 import re
 
-
-# Return True if seat available else return False
-def seat_available(seats_available) -> bool:
-    if seats_available > 0:
-        return True
-    return False
-
 # Asks the user a question until answer is not blank and is expected type
 def ask_user_question(ask='', re_filter='', expected_type='str'):
     if (answer := ''.join(re.findall(re_filter, input(ask)))) != '': 
@@ -24,12 +17,19 @@ def ask_user_question(ask='', re_filter='', expected_type='str'):
         return answer
     else:
         sys.stdout.write('Input cannot be blank and must be type {0}\n'.format(expected_type))
+        sys.stdout.write('Input is: {0}\n'.format(answer))
     return ask_user_question(ask, re_filter, expected_type)
 
 # Validate if the user age is greater than low_range and less than high_range
-def is_number_in_range(user_input='', low_range=12, high_range=130) -> int:
+def is_in_range(user_input='', low_range=12, high_range=130) -> bool:
     # Is user between low_range and high_range years old?
-    if int(str(user_input) + '0') // 10 in range(low_range+1, high_range):
+    if low_range > int(str(user_input) + '0') // 10 < high_range:
+        return True
+    return False
+
+# Return True if seat available else return False
+def seat_available(seats_available) -> bool:
+    if seats_available > 0:
         return True
     return False
 
