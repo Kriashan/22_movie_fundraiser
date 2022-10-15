3396f24ffec70d92d324144f0383d425eaae4989
Eldon Salter <es19090@melville-high.school.nz>
2022-10-16 12:46:31 +1300

    Combined snacks and tickets

--- test_ticket_price.py
+++ test_ticket_price.py
@@ -7,7 +7,7 @@ def order_snacks(user_name, user_age):
     available_snacks = {
                     "Popcorn": {"Price": 2.50, "Profit": 0.5},
                     "M&M": {"Price": 3.00, "Profit": 0.60},
-                    "Pita Chips": {"Price": 4.50, "Profit": 0.90},
+                    "Pita_chips": {"Price": 4.50, "Profit": 0.90},
                     "Orange juice": {"Price": 3.25, "Profit": 0.65},
                     "Water": {"Price": 2.00, "Profit": 0.40},
                     }
@@ -16,23 +16,34 @@ def order_snacks(user_name, user_age):
                       'Age': [],
                       'Popcorn': [],
                       'M&M': [],
-                      'Pita Chips': [],
+                      'Pita_chips': [],
                       'Orange juice': [],
                       'Water': [],
                       'Price': [],
                       'Profit': [] 
                      } # Name, age, Popcorn, M&M, etc, final_cost, profit
+    
+    ticket_brackets = {
+                    15: {'Price': 7.50, 'Profit': 2.50},
+                    64: {'Price': 10.50, 'Profit': 5.50},
+                    150: {'Price': 6.50, 'Profit': 1.50}
+                    }
 
     ordered_snacks['Name'].append(user_name)
     ordered_snacks['Age'].append(user_age)
     ordered_snacks['Popcorn'].append(0)
     ordered_snacks['M&M'].append(0)
-    ordered_snacks['Pita Chips'].append(0)
+    ordered_snacks['Pita_chips'].append(0)
     ordered_snacks['Orange juice'].append(0)
     ordered_snacks['Water'].append(0)
     ordered_snacks['Price'].append(0)
     ordered_snacks['Profit'].append(0)
 
+    for age in ticket_brackets:
+        if user_age < age:
+            ordered_snacks['Price'][len(ordered_snacks['Price'])-1] += ticket_brackets[age]['Price']
+            ordered_snacks['Profit'][len(ordered_snacks['Profit'])-1] += ticket_brackets[age]['Profit']
+            break
 
     while True:
         wants_snacks = ticketHandler.ask_user_question('Do you want snacks? yes or no? ', '[a-z]', 'str')
@@ -55,28 +66,37 @@ def order_snacks(user_name, user_age):
                         ordered_snacks['Profit'][len(ordered_snacks['Profit'])-1] += available_snacks[snackChoice]['Profit'] * snackQuantity
                     else:
                         ticketHandler.stdout('You can not order negative {0}'.format(snackChoice))
-                    ticketHandler.stdout(ordered_snacks)
+                    data = pandas.DataFrame(ordered_snacks)
+
+                    ticketHandler.stdout(data) 
                 else:
                     ticketHandler.stdout('That snack does not exist')
             ticketHandler.stdout('Snacks have been ordered')
+            ticketHandler.stdout('Final cost comes to ${0:.2f}'.format(ordered_snacks['Price'][len(ordered_snacks['Price'])-1]))
             break
         elif wants_snacks == 'no':
             ticketHandler.stdout('Snacks have not been ordered')
+            ticketHandler.stdout('Final cost comes to ${0:.2f}'.format(ordered_snacks['Price'][len(ordered_snacks['Price'])-1]))
             break
         ticketHandler.stdout('Please input yes or no')
 
 
 if __name__ == '__main__':
-
-    ticket_brackets = {
-                        15: {"Price": 7.50},
-                        64: {"Price": 10.50},
-                        65: {"Price": 6.50}
-                      }
+    pandas.options.display.max_rows = 9999
+    tickets_available = 150
 
     while True:
-        user_name = ticketHandler.ask_user_question('What is your first name?', '[A-Za-z]', 'str')
-        user_age = ticketHandler.ask_user_question('What is your age?', '[0-9]', 'int')
-        snacks_ordered = order_snacks(user_name, user_age)
+        ticketHandler.stdout('{0} tickets available'.format(tickets_available))
+        if ticketHandler.seat_available(tickets_available):
+            user_name = ticketHandler.ask_user_question('What is your first name?', '[A-Za-z]', 'str')
+            user_age = ticketHandler.ask_user_question('What is your age?', '[0-9]', 'int')
+            if ticketHandler.is_in_range(user_age):
+                ticketHandler.stdout('Unfortunately you are not within the age range required for this movie')
+                continue
+            snacks_ordered = order_snacks(user_name, user_age)
+            tickets_available -= 1
+        else:
+            ticketHandler.stdout("Sorry we are sold out")
+            break
 
         
